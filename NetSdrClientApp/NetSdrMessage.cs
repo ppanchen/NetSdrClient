2438b0854098bf867376129d62119b5319f3172f
ppanchen <mrpeterpan10@gmail.com>
2024-12-06 21:13:50 +0200

    - added NetSdrMessage class - covered by tests

---
+++ NetSdrClientApp/NetSdrMessage.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetSdrClientApp
+{
+    public static class NetSdrMessage
+    {
+        private const short _maxMessageLength = 8191;
+
+        public enum MsgTypes
+        {
+            SetControlItem,
+            CurrentControlItem,
+            ControlItemRange,
+            Ack,
+            DataItem0,
+            DataItem1,
+            DataItem2,
+            DataItem3
+        }
+
+        public enum ControlItemCodes
+        {
+            None = 0,
+            ReceiverState = 0x0018,
+            ReceiverFrequency = 0x0020
+        }
+
+        public static byte[] GetControlItemMessage(MsgTypes type, ControlItemCodes itemCode, byte[] parameters)
+        {
+            return GetMessage(type, itemCode, parameters);
+        }
+
+        public static byte[] GetDataItemMessage(MsgTypes type, byte[] parameters)
+        {
+            return GetMessage(type, ControlItemCodes.None, parameters);
+        }
+
+        private static byte[] GetMessage(MsgTypes type, ControlItemCodes itemCode, byte[] parameters)
+        {
+            var itemCodeBytes = Array.Empty<byte>();
+            if (itemCode != ControlItemCodes.None)
+            {
+                itemCodeBytes = BitConverter.GetBytes((ushort)itemCode);
+            }
+
+            var headerBytes = GetHeader(type, itemCodeBytes.Length + parameters.Length);
+
+            List<byte> msg = new List<byte>();
+            msg.AddRange(headerBytes);
+            msg.AddRange(itemCodeBytes);
+            msg.AddRange(parameters);
+
+            return msg.ToArray();
+        }
+
+        private static byte[] GetHeader(MsgTypes type, int msgLength)
+        {
+            int lengthWithHeader = msgLength + 2;
+
+            if (msgLength < 0 || lengthWithHeader > _maxMessageLength)
+            {
+                throw new ArgumentException("Message length exceeds allowed value");
+            }
+
+            return BitConverter.GetBytes((ushort)(lengthWithHeader + ((int)type << 13)));
+        }
+
+    }
+}
---
+++ NetSdrClientAppTests/NetSdrMessageTests.cs
@@ -0,0 +1,67 @@
+using NetSdrClientApp;
+
+namespace NetSdrClientAppTests
+{
+    public class NetSdrMessageTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetControlItemMessageTest()
+        {
+            //Arrange
+            var type = NetSdrMessage.MsgTypes.Ack;
+            var code = NetSdrMessage.ControlItemCodes.ReceiverState;
+            int parametersLength = 7500;
+
+            //Act
+            byte[] msg = NetSdrMessage.GetControlItemMessage(type, code, new byte[parametersLength]);
+
+            var headerBytes = msg.Take(2);
+            var codeBytes = msg.Skip(2).Take(2);
+            var parametersBytes = msg.Skip(4);
+
+            var num = BitConverter.ToUInt16(headerBytes.ToArray());
+            var actualType = (NetSdrMessage.MsgTypes)(num >> 13);
+            var actualLength = num - ((int)actualType << 13);
+            var actualCode = BitConverter.ToInt16(codeBytes.ToArray());
+
+            //Assert
+            Assert.That(headerBytes.Count(), Is.EqualTo(2));
+            Assert.That(msg.Length, Is.EqualTo(actualLength));
+            Assert.That(type, Is.EqualTo(actualType));
+
+            Assert.That(actualCode, Is.EqualTo((short)code));
+
+            Assert.That(parametersBytes.Count(), Is.EqualTo(parametersLength));
+        }
+
+        [Test]
+        public void GetDataItemMessageTest()
+        {
+            //Arrange
+            var type = NetSdrMessage.MsgTypes.DataItem2;
+            int parametersLength = 7500;
+
+            //Act
+            byte[] msg = NetSdrMessage.GetDataItemMessage(type, new byte[parametersLength]);
+
+            var headerBytes = msg.Take(2);
+            var parametersBytes = msg.Skip(2);
+
+            var num = BitConverter.ToUInt16(headerBytes.ToArray());
+            var actualType = (NetSdrMessage.MsgTypes)(num >> 13);
+            var actualLength = num - ((int)actualType << 13);
+
+            //Assert
+            Assert.That(headerBytes.Count(), Is.EqualTo(2));
+            Assert.That(msg.Length, Is.EqualTo(actualLength));
+            Assert.That(type, Is.EqualTo(actualType));
+
+            Assert.That(parametersBytes.Count(), Is.EqualTo(parametersLength));
+        }
+    }
+}
