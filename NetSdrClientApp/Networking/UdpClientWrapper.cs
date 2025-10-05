ed4c272d610adf4d181ffd5ad6922fe06e4b0058
Dark PC <malush1981@gmail.com>
2025-10-06 01:49:11 +0300

    fix issue: class 'UdpClientWrapper' overrides 'GetHashCode' and should therefore also override 'Equals'.
    add override for 'Equals' too.

--- NetSdrClientApp/Networking/UdpClientWrapper.cs
+++ NetSdrClientApp/Networking/UdpClientWrapper.cs
@@ -91,5 +91,16 @@ namespace NetSdrClientApp.Networking
 
             return BitConverter.ToInt32(hash, 0);
         }
+
+        public override bool Equals(object? obj)
+        {
+            if(obj is UdpClientWrapper other)
+            {
+                return _localEndPoint.Address.Equals(other._localEndPoint.Address) &&
+               _localEndPoint.Port == other._localEndPoint.Port;
+            }
+
+            return false;
+        }
     }
 }
