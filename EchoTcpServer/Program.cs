c269c85b27c48fd5aca4b671e3ae541713bffb19
Starenkiyy <9100759@stud.kai.edu.ua>
2025-11-13 01:30:01 +0200

    namespace NetSdrClient

--- EchoTcpServer/Program.cs
+++ EchoTcpServer/Program.cs
@@ -116,62 +116,65 @@ namespace NetSdrClient
     }
 }
 
-
-public class UdpTimedSender : IDisposable
+namespace NetSdrClient
 {
-    private readonly string _host;
-    private readonly int _port;
-    private readonly UdpClient _udpClient;
-    private Timer _timer;
 
-    public UdpTimedSender(string host, int port)
+    public class UdpTimedSender : IDisposable
     {
-        _host = host;
-        _port = port;
-        _udpClient = new UdpClient();
-    }
+        private readonly string _host;
+        private readonly int _port;
+        private readonly UdpClient _udpClient;
+        private Timer _timer;
 
-    public void StartSending(int intervalMilliseconds)
-    {
-        if (_timer != null)
-            throw new InvalidOperationException("Sender is already running.");
-
-        _timer = new Timer(SendMessageCallback, null, 0, intervalMilliseconds);
-    }
-
-    ushort i = 0;
-
-    private void SendMessageCallback(object state)
-    {
-        try
+        public UdpTimedSender(string host, int port)
         {
-            //dummy data
-            Random rnd = new Random();
-            byte[] samples = new byte[1024];
-            rnd.NextBytes(samples);
-            i++;
-
-            byte[] msg = (new byte[] { 0x04, 0x84 }).Concat(BitConverter.GetBytes(i)).Concat(samples).ToArray();
-            var endpoint = new IPEndPoint(IPAddress.Parse(_host), _port);
-
-            _udpClient.Send(msg, msg.Length, endpoint);
-            Console.WriteLine($"Message sent to {_host}:{_port} ");
+            _host = host;
+            _port = port;
+            _udpClient = new UdpClient();
         }
-        catch (Exception ex)
+
+        public void StartSending(int intervalMilliseconds)
         {
-            Console.WriteLine($"Error sending message: {ex.Message}");
+            if (_timer != null)
+                throw new InvalidOperationException("Sender is already running.");
+
+            _timer = new Timer(SendMessageCallback, null, 0, intervalMilliseconds);
         }
-    }
 
-    public void StopSending()
-    {
-        _timer?.Dispose();
-        _timer = null;
-    }
+        ushort i = 0;
 
-    public void Dispose()
-    {
-        StopSending();
-        _udpClient.Dispose();
+        private void SendMessageCallback(object state)
+        {
+            try
+            {
+                //dummy data
+                Random rnd = new Random();
+                byte[] samples = new byte[1024];
+                rnd.NextBytes(samples);
+                i++;
+
+                byte[] msg = (new byte[] { 0x04, 0x84 }).Concat(BitConverter.GetBytes(i)).Concat(samples).ToArray();
+                var endpoint = new IPEndPoint(IPAddress.Parse(_host), _port);
+
+                _udpClient.Send(msg, msg.Length, endpoint);
+                Console.WriteLine($"Message sent to {_host}:{_port} ");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error sending message: {ex.Message}");
+            }
+        }
+
+        public void StopSending()
+        {
+            _timer?.Dispose();
+            _timer = null;
+        }
+
+        public void Dispose()
+        {
+            StopSending();
+            _udpClient.Dispose();
+        }
     }
 }
