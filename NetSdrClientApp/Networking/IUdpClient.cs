f722e00495a97b0dad950db4ddce4404882fc152
Starenkiyy <9100759@stud.kai.edu.ua>
2025-11-13 01:41:04 +0200

    namespace NetSdrClientApp.Networking

--- NetSdrClientApp/Networking/IUdpClient.cs
+++ NetSdrClientApp/Networking/IUdpClient.cs
@@ -1,4 +1,11 @@
-﻿namespace NetSdrClientApp.Networking
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace NetSdrClientApp.Networking
 {
     public interface IUdpClient
     {
